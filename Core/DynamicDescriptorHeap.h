d649e2fe5c985a01ffa386b558ccc737054be9a1
Leusin <98079486+Leusin@users.noreply.github.com>
2024-07-20 13:46:24 +0900

    add: RootSignature class

---
+++ Core/DynamicDescriptorHeap.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "d3dx12.h"
+
+#include <wrl.h>
+
+#include <cstdint>
+#include <memory>
+#include <queue>
+
+class CommandList;
+// ID3D12RootSignature의 래퍼입니다.
+class RootSignature;
+
+class DynamicDescriptorHeap
+{
+};
